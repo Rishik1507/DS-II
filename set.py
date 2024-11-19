8c7b1424e53cad0a7a643441ae4b2ca4ce6ecbd2
Rishik1507 <microsoftrishik@gmail.com>
2024-11-19 21:41:42 +0530

    c

--- set.py
+++ set.py
@@ -3,3 +3,7 @@ print(st)
 st2={3,3,3,77,7,7}
 print(st2.difference(st))
 print(st2.symmetric_difference(st))
+set3={"milk","coffee"}
+set4={"juice","milk"}
+print(set3.union(set4))
+print(set3.intersection(set4))
